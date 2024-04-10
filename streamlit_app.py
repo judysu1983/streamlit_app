54185e92a7366d130b59bb6822b5c100b530d03b
judysu1983 <suli2921@gmail.com>
2024-04-09 19:51:43 -0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+def main():
+	st.title("hello, 我的数据 APP")
+	st.text("我爱 Python")
+	st.text("我爱 {}，{}".format("node.js"," Java"))
+	st.markdown("# 大乱斗")
+	st.markdown("+ iPhone :de:")
+	st.markdown("+ Android :earth_asia:")
+	st.video("https://www.youtube.com/watch?v=APa_G0_yz7I&list=PLS1QulWo1RIatQmigBRBEY4HQaOXxj7G6&pp=iAQB")
+	pass
+
+main()
